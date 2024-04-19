cdc80cadafc10273eeecb5d48fb328f0e0127c94
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-19 20:27:38 +0600

    script

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+
+// eta ekta object
+// const student= {
+
+
+// }; 
+
+// // eta hocche obeject er moddhe function call kora
+// const student= {
+//     fullName: "Intesar Shawon",
+//     marks: 95,
+//     printMarks: function () {
+//         console.log("marks=", this.marks); //function er vitor theke 95 marks print koranor jonno this use hoise
+//     },
+// }; 
+// // sob object er moddhe prototype thake, eta arekta object..,ekta object er moddhe arekta object jekhane onek kichu (methods,fuction, object) thake
+// __proto__ evabe prototype set kore
+
+
+// const employee={
+//     calTax1(){
+//         console.log("tax rate is 10%");
+//     },// eivabe function call kora jay, evabe beshi call hoy
+//     calTax2 : function () {
+//         console.log("tax rate is 10%");
+//     }// eivabeo kora jay, eivabe kon call hoy
+
+// };
+// const karanArjun={
+//     salary: 50000;
+// };
+
+
+
+
+const employee={
+    calTax1(){
+        console.log("tax rate is 10%");
+    }
+};
+const karanArjun={
+    salary: 50000;
+};
+
+
+
+//front end a lage na dekhe apatoto class stop ekhane 
+// https://www.youtube.com/watch?v=VlPiVmYuoqw
+// 8.28->9.21 a giye sikhe nibo lagle
+// biday 
+
