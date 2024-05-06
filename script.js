c0a966891cb5efeba707bfbe5af3514d75e37f72
INTESARSHAWON <intesarshawon@gmail.com>
2024-05-06 20:20:16 +0600

    class in js

--- script.js
+++ script.js
@@ -63,16 +63,52 @@
 
 
 
-const employee={
-    calTax1(){
-        console.log("tax rate is 10%");
+// const employee={
+//     calTax1(){
+//         console.log("tax rate is 10%");
+//     }
+// };
+// const karanArjun={
+//     salary: 50000,
+//     calTax1(){
+//         console.log("tax rate is 40%");
+//     }
+// };
+// karanArjun.__proto__ = employee;
+// // object ar prototype er jodi same method thake, tahole object er method use hobe
+
+
+
+
+//class in JS
+
+
+class ToyotaCar{
+    start(){
+        console.log("start");
     }
-};
-const karanArjun={
-    salary: 50000,
-    calTax1(){
-        console.log("tax rate is 40%");
+    stop(){
+        console.log("stop");
     }
-};
-karanArjun.__proto__ = employee;
-// object ar prototype er jodi same method thake, tahole object er method use hobe
+    //ekhane notun ekta function rakhtesi jetate ()er moddhe argument thakbe, nicher object gula eta use korte parbe 
+
+    setBrand(brand){
+        this.brandName = brand;
+        // this hocche ekhane object jetar jonno function create kora hocche
+        //ekhane this. er sathe je brandname ache eta object er property, but = er pashe jei bran ache ota ar bracket ermoddher ta argument
+    }
+}
+//eta ekta class, ekhane koma diye separate korte hoy na, niche dekhbo class theke kemne object create kore
+let fortuner = new ToyotaCar();
+// log a giye fortuner ke start stop sob e koraite parbo, karon class er moddhe jesob function ache, object access korte parbe
+fortuner.setBrand = "fortuner";
+
+let lexus = new ToyotaCar();
+// lexus o ei class tar sob use korte parbe, lexus.start() ba lexus.stop()- eshob likhe
+lexus.setBrand = "lexus";
+// upore this hocche ekhane object jetar jonno function create kora hocche, this er jayga ta fortune lexus eshob object nicche
+
+//uporer fortuner, lexus jokhon print dibo log a, auto constructor naam a special ekta method create hobe
+//ei constructor amra create na korleo JS auto create kore
+// tobe eta amra custom way te create korte pari jekono class er moddhe
+//niche oitai kortesi
